7e10497840900861e77cc2fee62f7123b1906c0d
Tate Bosler <tatebosler@gmail.com>
2026-04-04 13:31:40 -0500

    changes to deployment process

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+const assetPrefix = basePath || "";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: basePath || undefined,
+  assetPrefix: assetPrefix || undefined,
 };
 
 export default nextConfig;

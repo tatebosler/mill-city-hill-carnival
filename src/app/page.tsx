08d31edc861f2b0aa3efeedbca2776381acae99e
Tate Bosler <tatebosler@gmail.com>
2026-04-03 22:44:31 -0500

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Subtitle from './subtitle';
 
 export default function Home() {
   return (
-    <div className="text-center my-8">
-      <h1 className="text-3xl">Mill City Hill Carnival</h1>
+    <div className="text-center py-8 sm:py-24">
+      <h1 className="text-3xl sm:text-6xl">Mill City Hill Carnival</h1>
       <Subtitle />
     </div>
   );
--- src/app/subtitle.tsx
+++ src/app/subtitle.tsx
@@ -6,5 +6,5 @@ import { subtitleOptions, pickWeightedRandom } from './subtitle-options';
 export default function Subtitle() {
   const [text] = useState(() => pickWeightedRandom(subtitleOptions));
 
-  return <p id="subtitle" suppressHydrationWarning className="text-sm italic text-gray-300 mt-2">{text}</p>;
+  return <p id="subtitle" suppressHydrationWarning className="text-sm sm:text-2xl italic text-gray-300 mt-2 sm:mt-4">{text}</p>;
 }
